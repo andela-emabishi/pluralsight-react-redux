162763387914e0583b5a9f89bb7dd612f121e85f
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2017-02-10 11:23:09 +0300

    [Feature] Add courses route and CoursesPage component

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import {Route, IndexRoute} from 'react-router';
 import App from './components/app';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
+import CoursesPage from './components/course/CoursesPage';
 
 // Always load the app component as the react view controller- top level component
 // that passes down route params as props
@@ -12,5 +13,6 @@ export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
     <Route path="about" component={AboutPage} />
+    <Route path="courses" component={CoursesPage} />
   </Route>
 );
