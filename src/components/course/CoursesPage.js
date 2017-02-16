80cb85f9e106ae35cdf688a8d079340b2a6be1fb
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2017-02-17 00:36:45 +0300

    [Feature] Connect coursesPage component to redux with connect

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,4 +1,6 @@
 import React, {PropTypes} from 'react';
+import {connect} from 'react-redux'; // Used to creact components that can interact with redux
+import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends React.Component {
   // Initialise state in the component with a constructor
@@ -26,13 +28,23 @@ class CoursesPage extends React.Component {
   }
 
   onClickSave() {
-    alert(`Saving ${this.state.course.title}`);
+    // dispatch create course function
+    // this.props.dispatch(actionCreator(param))
+    this.props.dispatch(courseActions.createCourse(this.state.course));
+  }
+
+  courseRow(course, index) {
+    // Each iteration that renders a row must have a unique key
+    // In this case, the key maps to the index of each element in the array
+    // so that react can properly keep track of which row has been rednered
+    return <div key={index}>{course.title}</div>;
   }
 
   render() {
     return (
       <div>
         <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
         <h2>Add Course</h2>
         <input
           type="text"
@@ -50,4 +62,30 @@ class CoursesPage extends React.Component {
   }
 }
 
-export default CoursesPage;
+// propType validation
+CoursesPage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  dispatch: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+  // Should return portion of state to be exposed to component
+  return {
+    // Accessed in component as this.props.courses
+    // Portion of state mapped to props is state.courses
+    // courses key in state object influenced by courses parameter passed to combineReducers function
+    courses: state.courses
+  };
+}
+
+/*
+  * mapDispatchToProps used to delineate what actions you want exposed to your component
+  * If ommitted from call to the redux connect function, redux attaches a default dispatch property as a prop that
+  * makes it possible to dispatch all the actions available to the component
+*/
+
+// const connectStateAndProps = connect(mapStateToProps, mapDispatchToProps);
+// export default connectStateAndProps(CoursesPage);
+
+// Same as above
+export default connect(mapStateToProps)(CoursesPage);
