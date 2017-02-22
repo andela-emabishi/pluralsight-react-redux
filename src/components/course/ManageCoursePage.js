f8113c46c5c18ea6cf70ab1242a4a561fd3d5fe1
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2017-02-22 12:19:58 +0300

    [Feature] Create CourseForm,SelectInput and TextInput components

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -2,32 +2,52 @@
 * Container component
 */
 import React, {PropTypes} from 'react';
-import {connect} from 'react-redux';
+import {connect} from 'react-redux'; // Provides functionality for components to connect to the store
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseForm from './CourseForm';
 
 class ManageCoursePage extends React.Component {
   constructor(props, context) {
     super(props,context);
+
+    // Initialise the component state
+    this.state = {
+      course: Object.assign({}, this.props.course),
+      errors: {}
+    };
   }
 
   render() {
     return (
       <div>
-        <h1>Manage Course</h1>
+        <CourseForm
+          allAuthors={[]}
+          course={this.state.course}
+          errors={this.state.errors}
+        />
       </div>
     );
   }
 }
 
 ManageCoursePage.propTypes = {
-
+  course: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state,ownProps) {
+  let course = {
+    id: '',
+    watchHref: '',
+    title: '',
+    authorId: '',
+    length: '',
+    category: ''
+  };
   // props:state
+  // State accessible as this.props.course
   return {
-
+    course: course
   };
 }
 
